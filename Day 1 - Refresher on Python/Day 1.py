9686512e57573fdc10cee74e05e0d7225b7f433b
Konrad Kipczak <konrad.kipczak@gmail.com>
2018-08-29 17:27:49 -0400

    Completed Functions

--- Day 1 - Refresher on Python/Day 1.py
+++ Day 1 - Refresher on Python/Day 1.py
@@ -31,3 +31,31 @@ even = 7------3
 odd = 7-----3
 # even = 10
 # odd = 4 
+
+
+###########################
+### Exercise: Functions ###
+###########################
+
+### Basic Functions
+def round_to_two_places(num):
+    """Return the given number rounded to two decimal places. 
+    3.14
+    """
+    return round(num, 2)
+
+print(round_to_two_places(3.14159))
+
+
+### Time
+from time import time
+t = time()
+print(t, "seconds since the Epoch")
+
+
+### Timeouts
+from time import sleep
+duration = 5
+print("Getting sleepy. See you in", duration, "seconds")
+sleep(duration)
+print("I'm back. What did I miss?")
