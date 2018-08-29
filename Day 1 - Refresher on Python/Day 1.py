7290ab8ad8d6bfd9ad742667dfd6c9e8bad74ea8
Konrad Kipczak <konrad.kipczak@gmail.com>
2018-08-29 17:17:03 -0400

    Completed Syntax, Variables, and Numbers

--- Day 1 - Refresher on Python/Day 1.py
+++ Day 1 - Refresher on Python/Day 1.py
@@ -5,7 +5,9 @@
 ### Simple variable declerations
 color = "blue"
 print('color', color)
-
+odds = evens = []
+print('odds', odds)
+print('evens', evens)
 
 ### Simple math
 pi = 3.14159 # approximate
@@ -15,3 +17,17 @@ radius = 1.5
 # Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
 area = pi*(radius*radius)
 print('area', area)
+
+
+### Swapping variable values
+a = [1, 2, 3]
+b = [3, 2, 1]
+
+a, b = b, a
+
+
+### Ninety-nine dashes
+even = 7------3
+odd = 7-----3
+# even = 10
+# odd = 4 
