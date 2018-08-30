cae1aed481c9a07e9b02655eacf214fbd9863dfe
Konrad Kipczak <konrad.kipczak@gmail.com>
2018-08-30 17:20:55 -0400

    Another algorithm challenge completed. Ranked up in Python on CodeWars! :)

--- Day 1 and 2 - Refresher on Python/CodeWars Challenges.py
+++ Day 1 and 2 - Refresher on Python/CodeWars Challenges.py
@@ -19,3 +19,47 @@ def dig_pow(n, p):
         result += pow(int(digit),(ind+p))
     return result/n if result%n==0 else -1
 
+
+####################################
+#### 6 kyu - Your order, please  ###
+####################################
+
+# Your task is to sort a given string. Each word in the String will contain a single number. This number is the position the word should have in the result.
+
+# Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+# If the input String is empty, return an empty String. The words in the input String will only contain valid consecutive numbers.
+
+# For an input: "is2 Thi1s T4est 3a" the function should return "Thi1s is2 3a T4est"
+
+# your_order("is2 Thi1s T4est 3a")
+# [1] "Thi1s is2 3a T4est"
+
+### My Solution: (Terrible solution but at least I got there...)
+
+def order(sentence):
+  words = sentence.split(' ')
+  final = []
+  result = {}
+  output = []
+  numbers = ['1','2','3','4','5','6','7','8','9']
+  ind = 0
+  for val in words:  
+      for letter in val:
+          if letter in numbers:
+              final.append(numbers.index(letter))
+  for order in final:
+      result[order] = words[ind]
+      ind += 1
+      
+  for key, value in result.items():
+      output.append(value)
+      
+  return " ".join(output)
+
+# Here's a much cleaner solution... I have a lot of work to do
+
+def order(sentence):
+    return " ".join(sorted(sentence.split(), key=lambda x: int(filter(str.isdigit, x))))
+
+    
