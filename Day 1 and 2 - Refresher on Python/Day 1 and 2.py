15dd9dbb31d470cf8550f0d2d35db97dea78c323
Konrad Kipczak <konrad.kipczak@gmail.com>
2018-08-30 14:55:01 -0400

    Completed Lesson on Loops

--- Day 1 and 2 - Refresher on Python/Day 1 and 2.py
+++ Day 1 and 2 - Refresher on Python/Day 1 and 2.py
@@ -98,4 +98,65 @@ planets.index('Earth')
 t = (1, 2, 3) # Like lists but cannot be modified
 
 
+########################
+### Exercise: Loops  ###
+########################
+
+### Simple For Loop
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+for planet in planets:
+    print(planet, end=' ') # print all on same line
+
+### Loop over every character in a string
+s = 'steganograpHy is the practicE of conceaLing a file, message, image, or video within another fiLe, message, image, Or video.'
+msg = ''
+# print all the uppercase letters in s, one at a time
+for char in s:
+    if char.isupper():
+        print(char, end='')  # HELLO
+
+### Range of loop
+for i in range(5):
+    print("Doing important work. i =", i)
+Doing important work. i = 0
+Doing important work. i = 1
+Doing important work. i = 2
+Doing important work. i = 3
+Doing important work. i = 4
+
+### Enumerate
+nums = [1, 2, 4, 8, 16]
+def double_odds(nums):
+    for i, num in enumerate(nums):
+        if num % 2 == 1:
+            nums[i] = num * 2
+
+x = list(range(10))
+double_odds(x)
+x
+# [0, 2, 2, 6, 4, 10, 6, 14, 8, 18]
+
+### Iterating over Tuples
+nums = [
+    ('one', 1, 'I'),
+    ('two', 2, 'II'),
+    ('three', 3, 'III'),
+    ('four', 4, 'IV'),
+]
+
+for word, integer, roman_numeral in nums:
+    print(integer, word, roman_numeral, sep=' = ', end='; ')
+1 = one = I; 2 = two = II; 3 = three = III; 4 = four = IV; 
+
+### While Loop
+i = 0
+while i < 10:
+    print(i, end=' ')
+    i += 1
+0 1 2 3 4 5 6 7 8 9 
+
+### List Comprehension
+squares = [n**2 for n in range(10)]
+squares
+[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
