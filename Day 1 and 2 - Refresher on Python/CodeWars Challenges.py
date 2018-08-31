c16d1d4a40a167ab4bb29626d49e6c0f4eacebbb
Konrad Kipczak <konrad.kipczak@gmail.com>
2018-08-31 17:07:46 -0400

    Working on more CodeWars challendes

--- Day 1 and 2 - Refresher on Python/CodeWars Challenges.py
+++ Day 1 and 2 - Refresher on Python/CodeWars Challenges.py
@@ -59,7 +59,28 @@ def order(sentence):
 
 # Here's a much cleaner solution... I have a lot of work to do
 
-def order(sentence):
+def order2(sentence):
     return " ".join(sorted(sentence.split(), key=lambda x: int(filter(str.isdigit, x))))
 
-    
+
+############################
+#### 7 kyu - Two to One  ###
+############################
+
+# Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+
+# each taken only once - coming from s1 or s2. #Examples: ``` a = "xyaabbbccccdefww" b = "xxxxyyyyabklmopq" longest(a, b) -> "abcdefklmopqwxy"
+# a = "abcdefghijklmnopqrstuvwxyz" longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" ```
+
+def longest(s1, s2):
+    s3 = (s1 + s2)
+    listed = list(s3)
+    listed.sort()
+    final = []
+    for lets in listed:
+        if final.count(lets) == 0:
+            final.append(lets)
+    return ''.join(final)
+
+def longest2(a1, a2):
+    return "".join(sorted(set(a1 + a2)))
