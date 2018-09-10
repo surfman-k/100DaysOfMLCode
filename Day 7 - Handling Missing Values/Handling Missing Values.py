3129d8f9b411b23b346a067116e288f3a03f3d58
Konrad Kipczak <konrad.kipczak@gmail.com>
2018-09-10 17:23:51 -0400

    no message

--- Day 7 - Handling Missing Values/Handling Missing Values.py
+++ Day 7 - Handling Missing Values/Handling Missing Values.py
@@ -8,3 +8,51 @@ print(missing_val_count_by_column[missing_val_count_by_column > 0
 
 # Most libraries (including scikit-learn) will give you an error if you try to build a model using data with missing values. So you'll need to choose one of the strategies below.
 
+
+
+# If your data is in a DataFrame called original_data, you can drop columns with missing values. One way to do that is
+
+data_without_missing_values = original_data.dropna(axis=1)
+# In many cases, you'll have both a training dataset and a test dataset. You will want to drop the same columns in both DataFrames. In that case, you would write
+
+cols_with_missing = [col for col in original_data.columns 
+                                 if original_data[col].isnull().any()]
+redued_original_data = original_data.drop(cols_with_missing, axis=1)
+reduced_test_data = test_data.drop(cols_with_missing, axis=1)
+# If those columns had useful information (in the places that were not missing), your model loses access to this information when the column is dropped. Also, if your test data has missing values in places where your training data did not, this will result in an error.
+
+# So, it's somewhat usually not the best solution. However, it can be useful when most values in a column are missing.
+
+
+# A Better Option: Imputation¶
+# Imputation fills in the missing value with some number. The imputed value won't be exactly right in most cases, but it usually gives more accurate models than dropping the column entirely.
+
+# This is done with
+
+from sklearn.impute import SimpleImputer
+my_imputer = SimpleImputer()
+data_with_imputed_values = my_imputer.fit_transform(original_data)
+
+# The default behavior fills in the mean value for imputation. Statisticians have researched more complex strategies, but those complex strategies typically give no benefit once you plug the results into sophisticated machine learning models.
+# One (of many) nice things about Imputation is that it can be included in a scikit-learn Pipeline. Pipelines simplify model building, model validation and model deployment.
+
+
+
+
+# Imputation is the standard approach, and it usually works well. However, imputed values may by systematically above or below their actual values (which weren't collected in the dataset). Or rows with missing values may be unique in some other way. In that case, your model would make better predictions by considering which values were originally missing. Here's how it might look:
+
+# make copy to avoid changing original data (when Imputing)
+new_data = original_data.copy()
+
+# make new columns indicating what will be imputed
+cols_with_missing = (col for col in new_data.columns 
+                                 if new_data[col].isnull().any())
+for col in cols_with_missing:
+    new_data[col + '_was_missing'] = new_data[col].isnull()
+
+# Imputation
+my_imputer = SimpleImputer()
+new_data = pd.DataFrame(my_imputer.fit_transform(new_data))
+new_data.columns = original_data.columns
+
+# In some cases this approach will meaningfully improve results. In other cases, it doesn't help at all.
