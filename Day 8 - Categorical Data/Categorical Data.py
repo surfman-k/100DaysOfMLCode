94c6a084f23d6d2ef9d14cb33cb962d55a69c037
Konrad Kipczak <konrad.kipczak@gmail.com>
2018-09-18 22:16:58 -0400

    back at it!

--- Day 8 - Categorical Data/Categorical Data.py
+++ Day 8 - Categorical Data/Categorical Data.py
@@ -1,1 +1,10 @@
 # Categorical Data
+
+# Categorical data is data that takes only a limited number of values.
+# For example, if you people responded to a survey about which what brand of car they owned, the result would be categorical (because the answers would be things like Honda, Toyota, Ford, None, etc.). Responses fall into a fixed set of categories.
+# You will get an error if you try to plug these variables into most machine learning models in Python without "encoding" them first. Here we'll show the most popular method for encoding categorical variables.
+
+# One hot encoding is the most widespread approach, and it works very well unless your categorical variable takes on a large number of values (i.e. you generally won't it for variables taking more than 15 different values. It'd be a poor choice in some cases with fewer values, though that varies.)
+# One hot encoding creates new (binary) columns, indicating the presence of each possible value from the original data. 
+
+
