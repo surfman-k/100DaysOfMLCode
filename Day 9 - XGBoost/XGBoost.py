dab3ee32e31084828842c3af924482a6f2b4ae3d
Konrad Kipczak <konrad.kipczak@gmail.com>
2018-09-19 09:40:44 -0400

    XGBoost part 1

--- Day 9 - XGBoost/XGBoost.py
+++ Day 9 - XGBoost/XGBoost.py
@@ -1,1 +1,51 @@
 ### XGBoost
+
+# XGBoost is the leading model for working with standard tabular data (the type of data you store in Pandas DataFrames, as opposed to more exotic types of data like images and videos). XGBoost models dominate many Kaggle competitions.
+
+# To reach peak accuracy, XGBoost models require more knowledge and model tuning than techniques like Random Forest. After this tutorial, you'ill be able to
+
+# Follow the full modeling workflow with XGBoost
+# Fine-tune XGBoost models for optimal performance
+# XGBoost is an implementation of the Gradient Boosted Decision Trees algorithm (scikit-learn has another version of this algorithm, but XGBoost has some technical advantages.) What is Gradient Boosted Decision Trees? We'll walk through a diagram.
+
+
+# We go through cycles that repeatedly builds new models and combines them into an ensemble model. We start the cycle by calculating the errors for each observation in the dataset. We then build a new model to predict those. We add predictions from this error-predicting model to the "ensemble of models."
+
+# To make a prediction, we add the predictions from all previous models. We can use these predictions to calculate new errors, build the next model, and add it to the ensemble.
+
+# There's one piece outside that cycle. We need some base prediction to start the cycle. In practice, the initial predictions can be pretty naive. Even if it's predictions are wildly inaccurate, subsequent additions to the ensemble will address those errors.
+
+# This process may sound complicated, but the code to use it is straightforward. We'll fill in some additional explanatory details in the model tuning section below.
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import Imputer
+
+data = pd.read_csv('../input/train.csv')
+data.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = data.SalePrice
+X = data.drop(['SalePrice'], axis=1).select_dtypes(exclude=['object'])
+train_X, test_X, train_y, test_y = train_test_split(X.as_matrix(), y.as_matrix(), test_size=0.25)
+
+my_imputer = Imputer()
+train_X = my_imputer.fit_transform(train_X)
+test_X = my_imputer.transform(test_X)
+
+# /opt/conda/lib/python3.6/site-packages/ipykernel_launcher.py:9: FutureWarning: Method .as_matrix will be removed in a future version. Use .values instead.
+#   if __name__ == '__main__':
+# /opt/conda/lib/python3.6/site-packages/sklearn/utils/deprecation.py:58: DeprecationWarning: Class Imputer is deprecated; Imputer was deprecated in version 0.20 and will be removed in 0.22. Import impute.SimpleImputer from sklearn instead.
+#   warnings.warn(msg, category=DeprecationWarning)
+
+from xgboost import XGBRegressor
+
+my_model = XGBRegressor()
+# Add silent=True to avoid printing out updates with each cycle
+my_model.fit(train_X, train_y, verbose=False)
+
+# make predictions
+predictions = my_model.predict(test_X)
+
+from sklearn.metrics import mean_absolute_error
+print("Mean Absolute Error : " + str(mean_absolute_error(predictions, test_y)))
+# Mean Absolute Error : 17543.750299657535
+
