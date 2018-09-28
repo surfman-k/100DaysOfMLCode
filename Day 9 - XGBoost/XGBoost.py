90165493f979df2f2c671122bc1b710cde6a7caa
Konrad Kipczak <konrad.kipczak@gmail.com>
2018-09-28 12:59:37 -0400

    Finished XGBoost

--- Day 9 - XGBoost/XGBoost.py
+++ Day 9 - XGBoost/XGBoost.py
@@ -49,3 +49,49 @@ from sklearn.metrics import mean_absolute_error
 print("Mean Absolute Error : " + str(mean_absolute_error(predictions, test_y)))
 # Mean Absolute Error : 17543.750299657535
 
+
+
+# Model Tuning
+# XGBoost has a few parameters that can dramatically affect your model's accuracy and training speed. The first parameters you should understand are:
+
+# n_estimators and early_stopping_rounds
+# n_estimators specifies how many times to go through the modeling cycle described above.
+
+# In the underfitting vs overfitting graph, n_estimators moves you further to the right. Too low a value causes underfitting, which is inaccurate predictions on both training data and new data. Too large a value causes overfitting, which is accurate predictions on training data, but inaccurate predictions on new data (which is what we care about). You can experiment with your dataset to find the ideal. Typical values range from 100-1000, though this depends a lot on the learning rate discussed below.
+
+# The argument early_stopping_rounds offers a way to automatically find the ideal value. Early stopping causes the model to stop iterating when the validation score stops improving, even if we aren't at the hard stop for n_estimators. It's smart to set a high value for n_estimators and then use early_stopping_rounds to find the optimal time to stop iterating.
+
+# Since random chance sometimes causes a single round where validation scores don't improve, you need to specify a number for how many rounds of straight deterioration to allow before stopping. early_stopping_rounds = 5 is a reasonable value. Thus we stop after 5 straight rounds of deteriorating validation scores.
+
+my_model = XGBRegressor(n_estimators=1000)
+my_model.fit(train_X, train_y, early_stopping_rounds=5, 
+             eval_set=[(test_X, test_y)], verbose=False)
+# XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
+#        colsample_bytree=1, gamma=0, learning_rate=0.1, max_delta_step=0,
+#        max_depth=3, min_child_weight=1, missing=None, n_estimators=1000,
+#        n_jobs=1, nthread=None, objective='reg:linear', random_state=0,
+#        reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=None,
+#        silent=True, subsample=1)
+
+# When using early_stopping_rounds, you need to set aside some of your data for checking the number of rounds to use. If you later want to fit a model with all of your data, set n_estimators to whatever value you found to be optimal when run with early stopping.
+
+# learning_rate
+# Here's a subtle but important trick for better XGBoost models:
+
+# Instead of getting predictions by simply adding up the predictions from each component model, we will multiply the predictions from each model by a small number before adding them in. This means each tree we add to the ensemble helps us less. In practice, this reduces the model's propensity to overfit.
+
+# So, you can use a higher value of n_estimators without overfitting. If you use early stopping, the appropriate number of trees will be set automatically.
+
+# In general, a small learning rate (and large number of estimators) will yield more accurate XGBoost models, though it will also take the model longer to train since it does more iterations through the cycle.
+
+# Modifying the example above to include a learing rate would yield the following code:
+my_model = XGBRegressor(n_estimators=1000, learning_rate=0.05)
+my_model.fit(train_X, train_y, early_stopping_rounds=5, 
+             eval_set=[(test_X, test_y)], verbose=False)
+
+# n_jobs
+# On larger datasets where runtime is a consideration, you can use parallelism to build your models faster. It's common to set the parameter n_jobs equal to the number of cores on your machine. On smaller datasets, this won't help.
+
+# The resulting model won't be any better, so micro-optimizing for fitting time is typically nothing but a distraction. But, it's useful in large datasets where you would otherwise spend a long time waiting during the fit command.
+
+# XGBoost has a multitude of other parameters, but these will go a very long way in helping you fine-tune your XGBoost model for optimal performance.
